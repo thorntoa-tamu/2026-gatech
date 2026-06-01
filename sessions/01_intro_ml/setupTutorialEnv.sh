cb46f3e25e15ae89625ea7d91ed28b8ca97581c0
roger-huang-1 <roger_huang@berkeley.edu>
2026-05-31 19:59:52 -0700

    Update to environment setup

---
+++ sessions/01_intro_ml/setupTutorialEnv.sh
@@ -0,0 +1,3 @@
+module load pytorch/2.6.0
+pip install uproot
+pip install torchinfo
--- setupTutorialEnv.sh
+++
@@ -1,3 +0,0 @@
-module load conda
-conda activate /global/common/software/ntrain6/envs/ml4fp2026day1
-python -m ipykernel install --user --name ml4fp2026day1 --display-name "ML4FP2026_Day1"
